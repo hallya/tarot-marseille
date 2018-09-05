597cbe570f1e66f32720f6cd4c2d7cb918d10309
Lucien  Dulac <lucien.dulac@ekino.com>
2018-09-05 17:20:16 +0200

    received props more accurate, variables removed at this occasion

--- src/components/Alliance/alliance.js
+++ src/components/Alliance/alliance.js
@@ -2,22 +2,32 @@ import React from 'react';
 import './alliance.scss';
 
 
-const Alliance = (props) => {
-  const { m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14 } = props.state,
-    a = [m6, m12, m1, m8, m9, m2, m5, m4, m7, m3, m11, m10],
-    b = [m13, m14],
-    shortNamesA = ['m6', 'm12', 'm1', 'm8', 'm9', 'm2', 'm5', 'm4', 'm7', 'm3', 'm11', 'm10'],
-    shortNamesB = ['m13', 'm14'],
-    fullNamesA = ['Maison 6', 'Maison 12', 'Maison 1', 'Maison 8', 'Maison 9', 'Maison 2', 'Maison 5', 'Maison 4', 'Maison 7', 'Maison 3', 'Maison 11', 'Maison 10'],
-    fullNamesB = ['Maison 13', 'Maison 14'],
-    slideAndShow = props.state.m14 >= 0 ? '' : 'hide-arcanes',
-    shadowAnimation = props.state.m14 >= 0 ? 'anim-shadow' : '',
-    showDescription = props.state.m14 >= 0 ? 'show-description' : '';
+const Alliance = ({
+  handleUser,
+  majors,
+  minors,
+  firstname,
+  lastname,
+  firstnamePartner,
+  lastnamePartner,
+  day,
+  month,
+  year,
+  dayPartner,
+  monthPartner,
+  yearPartner,
+  firstSetHouses,
+  secondSetHouses,
+}) => {
+  const
+    slideAndShow = secondSetHouses[secondSetHouses.length - 1] >= 0 ? '' : 'hide-arcanes',
+    shadowAnimation = secondSetHouses[secondSetHouses.length - 1] >= 0 ? 'anim-shadow' : '',
+    showDescription = secondSetHouses[secondSetHouses.length - 1] >= 0 ? 'show-description' : '';
 
   return (
     <main className='arcane'>
       <section className='settings'>
-        <form onSubmit={props.handleUser}>
+        <form onSubmit={handleUser}>
           <input className='user' type='text' autoComplete='given-name' name='firstname' placeholder='Prénom'></input>
           <input className='user' type='text' autoComplete='family-name' name='lastname' placeholder='Nom'></input>
           <input className='user' type='date' name='birthday' min="0001-01-01" max="9999-12-31"></input>
@@ -30,36 +40,36 @@ const Alliance = (props) => {
           <button className='user' type="submit">Valider</button>
         </form>
       </section>
-      <section className={`results ${slideAndShow}`} ref={props.print}>
+      <section className={`results ${slideAndShow}`}>
         <ul className='first'>
           {
-            a.map((carte, i) => <li key={i} className={shortNamesA[i]}>
-              <img src={props.state.cards[a[i]]} className={shadowAnimation} alt=''/>
-                <p className={`description ${showDescription}`}>{fullNamesA[i]}</p>
+            firstSetHouses.map((card, i) => <li key={i} className={`m${i + 1}`}>
+              <img src={majors[firstSetHouses[i]]} className={shadowAnimation} alt=''/>
+                <p className={`description ${showDescription}`}>{`Maison ${i + 1}`}</p>
               </li>
             )
           }
         </ul>
         <ul className='second'>
           <li className='fullname' >
-            {props.state.firstname} {props.state.lastname}
+            {firstname} {lastname}
             <br />
-            né(e) le {props.state.day}.{props.state.month}.{props.state.year}
+            né(e) le {day}.{month}.{year}
             <br />
-            {props.state.firstname ? 'et' : ''}
+            {firstnamePartner ? 'et' : ''}
             <br />
-            {props.state.firstnamePartner} {props.state.lastnamePartner}
+            {firstnamePartner} {lastnamePartner}
             <br />
             {
-              props.state.firstnamePartner
-                ? `né(e) le ${props.state.dayPartner}.${props.state.monthPartner}.${props.state.yearPartner}`
+              firstnamePartner
+                ? `né(e) le ${dayPartner}.${monthPartner}.${yearPartner}`
                 : ''
             }
           </li>
           {
-            b.map((carte, i) => <li key={i} className={shortNamesB[i]}>
-              <img src={i === 0 ? props.state.cards[b[i]] : props.state.minors[b[i]]} className={shadowAnimation} alt='' />
-              <p className={`description ${showDescription}`}>{fullNamesB[i]}</p>
+            secondSetHouses.map((card, i) => <li key={i} className={`m${i + 13}`}>
+              <img src={i === 0 ? majors[secondSetHouses[i]] : minors[secondSetHouses[i]]} className={shadowAnimation} alt='' />
+              <p className={`description ${showDescription}`}>{`Maison ${i + 13}`}</p>
             </li>)
           }
         </ul>
