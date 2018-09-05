8e535c5b24391f50e2a5fb33595a40c4a9dbce77
Lucien  Dulac <lucien.dulac@ekino.com>
2018-09-05 17:29:02 +0200

    indentation modified, id + onClick event removed, first tab link renamed

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,18 +2,32 @@ import React from 'react';
 import './header.scss';
 import { Link } from 'react-router-dom';
 
-const Header = (props) => {
+const Header = ({activeTab}) => {
   return (
     <header>
       <nav>
         <ul>
-          <li id='arcanes' onClick={() => props.goToTab('arcanes')} className={props.activeTab.arcanes ? 'selected':''}><Link to='/tarot-marseille'>Arcanes</Link></li>
-          <li id='alliance' onClick={() => props.goToTab('alliance')} className={props.activeTab.alliance ? 'selected':''}><Link to='/alliance'>Alliance</Link></li>
-          <li id='miroirs13' onClick={() => props.goToTab('miroirs13')} className={props.activeTab.miroirs13 ? 'selected':''}><Link to='/miroirs-13'>Miroirs 13</Link></li>
-          <li id='miroirs17' onClick={() => props.goToTab('miroirs17')} className={props.activeTab.miroirs17 ? 'selected':''}><Link to='/miroirs-17'>Miroirs 17</Link></li>
-          <li id='miroirs22' onClick={() => props.goToTab('miroirs22')} className={props.activeTab.miroirs22 ? 'selected':''}><Link to='/miroirs-22'>Miroirs 22</Link></li>
-          <li id='voies' onClick={() => props.goToTab('voies')} className={props.activeTab.voies ? 'selected':''}><Link to='/voies-boucles'>Voies & boucles</Link></li>
-          <li id='anneesPersonnelles' onClick={() => props.goToTab('anneesPersonnelles')} className={props.activeTab.anneesPersonnelles ? 'selected':''}><Link to='/annees-personnelles'>Années personnelles</Link></li>
+          <li className={activeTab === '/arcanes' ? 'selected':''}>
+            <Link to='/arcanes'>Arcanes</Link>
+          </li>
+          <li className={activeTab === '/alliance' ? 'selected':''}>
+            <Link to='/alliance'>Alliance</Link>
+          </li>
+          <li className={activeTab === '/miroirs-13' ? 'selected':''}>
+            <Link to='/miroirs-13'>Miroirs 13</Link>
+          </li>
+          <li className={activeTab === '/miroirs-17' ? 'selected':''}>
+            <Link to='/miroirs-17'>Miroirs 17</Link>
+          </li>g
+          <li className={activeTab === '/miroirs-22' ? 'selected':''}>
+            <Link to='/miroirs-22'>Miroirs 22</Link>
+          </li>
+          <li className={activeTab === '/voies-boucles' ? 'selected':''}>
+            <Link to='/voies-boucles'>Voies & boucles</Link>
+          </li>
+          <li className={activeTab === '/annees-personnelles' ? 'selected':''}>
+            <Link to='/annees-personnelles'>Années personnelles</Link>
+          </li>
         </ul>
       </nav>
     </header>
