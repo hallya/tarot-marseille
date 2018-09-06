2f42beb820624958aff285838e860f28a4fe34b6
Lucien  Dulac <lucien.dulac@ekino.com>
2018-09-06 15:54:42 +0200

    new build

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import {HashRouter as Router } from 'react-router-dom'
 import App from './App';
 
 ReactDOM.render(
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <App />
   </Router>, document.getElementById('root'));
