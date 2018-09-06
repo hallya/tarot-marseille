dfb69ae3c690e7b1527acbd6fab0a19a1ab3efbb
Lucien  Dulac <lucien.dulac@ekino.com>
2018-09-06 10:31:39 +0200

    base path contextualised

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import './index.scss';
 import {BrowserRouter as Router } from 'react-router-dom'
 import App from './App';
 
+const rootPath = process.env.NODE_ENV === 'production' ? '/tarot-marseille' : '/';
+
 ReactDOM.render(
-  <Router>
+  <Router basename={rootPath}>
     <App />
   </Router>, document.getElementById('root'));
