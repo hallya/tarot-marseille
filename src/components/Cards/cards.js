d1d61c90e0afba1c1d8c7ba9efe033753894d285
Lucien  Dulac <lucien.dulac@ekino.com>
2018-09-07 17:35:24 +0200

    proptypes added

--- src/components/Cards/cards.js
+++ src/components/Cards/cards.js
@@ -24,7 +24,7 @@ const Cards = ({
   <section className={`results ${slideAndShow}`}>
     <ul className='first'>
       {
-        firstSetHouses.map((card, i) => <li key={i} className={`m${i + 1}`}>
+        firstSetHouses && firstSetHouses.map((card, i) => <li key={i} className={`m${i + 1}`}>
           <img src={majors[firstSetHouses[i]]} className={shadowAnimation} alt=''/>
             <p className={`description ${showDescription}`}>{`Maison ${i + 1}`}</p>
           </li>
@@ -34,17 +34,15 @@ const Cards = ({
     <ul className='second'>
       <li className='fullname' >
         <Signature firstname={firstname} lastname={lastname} day={day} month={month} year={year} />
-        <br />
         {
           yearPartner && 'et'
         }
-        <br/>  
         {
           yearPartner && <Signature firstname={firstnamePartner} lastname={lastnamePartner} day={dayPartner} month={monthPartner} year={yearPartner} />
         }  
       </li>
       {
-        secondSetHouses.map((card, i) => <li key={i} className={`m${i + 13}`}>
+        secondSetHouses &&  secondSetHouses.map((card, i) => <li key={i} className={`m${i + 13}`}>
           <img src={i === 0 ? majors[secondSetHouses[i]] : minors[secondSetHouses[i]]} className={shadowAnimation} alt='' />
           <p className={`description ${showDescription}`}>{`Maison ${i + 13}`}</p>
         </li>)
@@ -54,6 +52,22 @@ const Cards = ({
 )
 
 Cards.propTypes = {
-  slideAndShow: PropTypes.string
+  slideAndShow: PropTypes.string,
+  firstSetHouses: PropTypes.array,
+  secondSetHouses: PropTypes.array,
+  firstname: PropTypes.string,
+  lastname: PropTypes.string,
+  firstnamePartner: PropTypes.string,
+  lastnamePartner: PropTypes.string,
+  day: PropTypes.number,
+  month: PropTypes.number,
+  year: PropTypes.number,
+  dayPartner: PropTypes.number,
+  monthPartner: PropTypes.number,
+  yearPartner: PropTypes.number,
+  shadowAnimation: PropTypes.string,
+  showDescription: PropTypes.string,
+  majors: PropTypes.array,
+  minors: PropTypes.array,
 }
 export default Cards;
