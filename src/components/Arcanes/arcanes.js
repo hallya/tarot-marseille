d87e785ba9521033203fe7196e4ac8a89ff692b2
Lucien  Dulac <lucien.dulac@ekino.com>
2018-09-07 17:35:56 +0200

    props received and passed modified

--- src/components/Arcanes/arcanes.js
+++ src/components/Arcanes/arcanes.js
@@ -1,61 +1,30 @@
 import React from 'react';
 import './arcanes.scss';
 import PropTypes from 'prop-types';
+import UserField from 'Components/User/fieldset';
+import Cards from 'Components/Cards/cards';
 
-const Arcanes = ({
-  handleUser,
-  majors,
-  minors,
-  firstname,
-  lastname,
-  day,
-  month,
-  year,
-  firstSetHouses,
-  secondSetHouses
-}) => {
+const Arcanes = (props) => {
   const
-    slideAndShow = !year || year === 0 ? 'hide-arcanes' : '',
-    shadowAnimation = secondSetHouses[secondSetHouses.length - 1] >= 0 ? 'anim-shadow' : '',
-    showDescription = secondSetHouses[secondSetHouses.length - 1] >= 0 ? 'show-description' : '';
+    { handleUser, year } = props,
+    slideAndShow = year === undefined ? 'hide-arcanes' : '',
+    shadowAnimation = year !== undefined ? 'anim-shadow' : '',
+    showDescription = year !== undefined ? 'show-description' : '';
 
   return (
     <main className='arcane'>
       <section className='settings'>
         <form onSubmit={handleUser}>
-          <input className='user' type='text' autoComplete='given-name' name='firstname' placeholder='Prenom'></input>
-          <input className='user' type='text' autoComplete='family-name' name='lastname' placeholder='Nom'></input>
-          <input required className='user' type='date' name='birthday' min="0001-01-01" max="3000-12-31"></input>
-          <input className='user' type='number' name='currentYear' placeholder='Année courante'></input>
+          <UserField firstname={'firstname'} lastname={'lastname'} birthday={'birthday'}/>
+          <input className='user' type='number' name='currentYear' placeholder={new Date().getFullYear()}></input>
           <button className='user' type="submit">Valider</button>
         </form>
       </section>
-      <section className={`results ${slideAndShow}`}>
-        <ul className='first'>
-          {
-            firstSetHouses.map((card, i) => <li key={i} className={`m${i + 1}`}>
-              <img src={majors[firstSetHouses[i]]} className={shadowAnimation} alt=''/>
-                <p className={`description ${showDescription}`}>{`Maison ${i + 1}`}</p>
-              </li>
-            )
-          }
-        </ul>
-        <ul className='second'>
-          <li className='fullname' >
-            {firstname}
-            <br />
-            {lastname}
-            <br />
-            né(e) le {day}.{month}.{year}
-          </li>
-          {
-            secondSetHouses.map((card, i) => <li key={i} className={`m${i + 13}`}>
-              <img src={i === 0 ? majors[secondSetHouses[i]] : minors[secondSetHouses[i]]} className={shadowAnimation} alt='' />
-              <p className={`description ${showDescription}`}>{`Maison ${i + 13}`}</p>
-            </li>)
-          }
-        </ul>
-      </section>
+      <Cards
+        slideAndShow={slideAndShow}
+        shadowAnimation={shadowAnimation}
+        showDescription={showDescription}
+        {...props} />
     </main>
   );  
 }
