2f3c58d6515ce316c866183ea4c4e43cb3b45cd2
Lucien  Dulac <lucien.dulac@ekino.com>
2018-09-07 17:31:30 +0200

    creation of two arrays of houses

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -55,8 +55,11 @@ export const getHouses = (state) => {
     m11 = reduceNumberIfNecessary(m3 + m7 + m10, 22),
     m12 = reduceNumberIfNecessary(m4 + m2 + m6, 22),
     m13 = reduceNumberIfNecessary(m11 + m3 + m5 + m1 + m12 + m9 + m2 + m5 + m4, 22),
-    m14 = reduceNumberIfNecessary(m1 + m2 + m3 + m4, 56);
-    return { m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14 };
+    m14 = reduceNumberIfNecessary(m1 + m2 + m3 + m4, 56),
+    firstSetHouses = [m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12],
+    secondSetHouses = [m13, m14];
+  
+  return { m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, firstSetHouses, secondSetHouses };
 }
 
 const getMatchs = (a, b, houses) => {
