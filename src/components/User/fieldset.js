fc5b9b4842ecbd700944eebc491a51e061d2c4a4
Lucien Dulac <coulterfeith@live.fr>
2018-09-15 21:52:11 +0200

    placeholder added for safari date input

--- src/components/User/fieldset.js
+++ src/components/User/fieldset.js
@@ -5,7 +5,7 @@ const UserField = ({firstname, lastname, birthday}) => (
   <fieldset>
     <input className='user' type='text' autoComplete='given-name' name={firstname} placeholder='Prénom'></input>
     <input className='user' type='text' autoComplete='family-name' name={lastname} placeholder='Nom'></input>
-    <input required className='user' type='date' name={birthday} min="0001-01-01" max="9999-12-31"></input>
+    <input required className='user' type='date' name={birthday} min="0001-01-01" max="9999-12-31" placeholder="jj/mm/aaaa"></input>
   </fieldset>
 )
 
