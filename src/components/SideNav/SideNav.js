b8b49571e87e9cab714555461c34d0c2ecc25a5d
Lucien Dulac <coulterfeith@live.fr>
2018-10-07 13:48:08 +0200

    New Component dealing only with navigation

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -1,28 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { subRoutes } from './config';
-import Form from '../Form/Form';
 import './SideNav.scss';
 
-const SideNav = (props) => (
-  <section className="sidenav">
-    <Form {...props}>
-      { props.children }
-    </Form>
-    <nav>
-      <ul className="sidenav">
-        {
-          subRoutes.map(({ path, name }) => (
-            <li key={`link${path}`}>
-              <NavLink  activeClassName="rendered" to={props.match.url + path}>
-                {name}
-              </NavLink>
-            </li>
-          ))
-        }
-      </ul>
-    </nav>
-  </section>
+const SideNav = ({match}) => (
+  <nav>
+    <ul className="sidenav">
+      {
+        subRoutes.map(({ path, name }) => (
+          <li key={`link${path}`}>
+            <NavLink  activeClassName="rendered" to={match.url + path}>
+              {name}
+            </NavLink>
+          </li>
+        ))
+      }
+    </ul>
+  </nav>
 )
 
 export default SideNav;
