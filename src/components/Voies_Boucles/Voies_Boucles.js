b41c2adf51ccf342ee5f76063d4afa05682b42d7
Lucien Dulac <coulterfeith@live.fr>
2018-10-08 20:54:44 +0200

    component divided in sub-component, refactoring code and scss values moved in dedicated scss sub-component files

--- src/components/Voies_Boucles/Voies_Boucles.js
+++ src/components/Voies_Boucles/Voies_Boucles.js
@@ -1,37 +1,16 @@
 import React from 'react';
-import './Voies_Boucles.scss';
 import PropTypes from 'prop-types';
+import Voies from '../Voies/Voies';
+import Boucles from '../Boucles/Boucles';
+import './Voies_Boucles.scss';
 
 const VoiesEtBoucles = ({voies, boucles}) => { 
   return (
     <article className='voies-boucles'>
-      {
-        voies && <h2 className="voies" >Voies</h2>
-      }
-      {
-        voies
-          ? <ul className="list-voies">
-              {
-                voies.length
-                  ? voies.map((voie, index) => <li key={index}>{voie}</li>)
-                  : <li>Pas de voie . . .</li>
-              }
-            </ul>
-          : <p>Entrez votre date de naissance dans l'onglet Arcanes ou Alliance</p>
-      }
-      {
-        boucles && <h2 className="boucles">Boucles</h2>
-      }
-      {
-        boucles
-          && <ul className="list-boucles">
-              {
-                boucles.length
-                  ? boucles.map((boucle, index) => <li key={index}>{boucle}</li>)
-                  : <li>Pas de boucles . . .</li>
-              }
-            </ul>
-      }
+      <h2 className="voies" >Voies</h2>
+      <Voies voies={voies}/>
+      <h2 className="boucles">Boucles</h2>
+      <Boucles boucles={boucles}/>
     </article>
   )
 }
