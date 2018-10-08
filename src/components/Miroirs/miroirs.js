7b5a3be134b78d03ec70a6ac6564fe7ec6c575f2
Lucien Dulac <coulterfeith@live.fr>
2018-10-08 20:49:46 +0200

    verifications that were not required has been removed, design align to other subPages

--- src/components/Miroirs/miroirs.js
+++ src/components/Miroirs/miroirs.js
@@ -3,39 +3,35 @@ import PropTypes from 'prop-types';
 import { majors } from '../../utils/img';
 import './miroirs.scss';
 
-const Miroirs= ({ miroirs }) => {
+const Miroirs = ({ miroirs, path }) => {
   return (
     <article className='miroirs'>
-      {
-        miroirs
-          ? <ul className='miroirs'>
-            {
-              miroirs.length
-                ? miroirs.map((value, index) => (
-                  <li key={index}>
-                    {
-                      value.images.map((image, index) => <img src={majors[image]} className='miroir-image' key={index} alt="" />)
-                    }
-                    <p className='miroir-title' >{value.title}</p>
-                    <ul className='combinaison'>
-                      {
-                        value.combinaisons.map((combinaison, index) => (
-                          <li className='combinaison-maison' key={index}>{combinaison}</li>
-                        ))
-                      }
-                    </ul>
-                  </li> ))
-                : <li>Pas de miroir . . .</li>
-            }
-            </ul>
-          : <p>Entrez votre date de naissance dans l'onglet Arcanes ou Alliance</p>
+      <ul className='miroirs'>
+      { 
+        miroirs[path].length
+          ? miroirs[path].map((value, index) => (
+            <li key={index}>
+              {
+                value.images.map((image, index) => <img src={majors[image]} className='miroir-image' key={index} alt="" />)
+              }
+              <p className='miroir-title' >{value.title}</p>
+              <ul className='combinaison'>
+                {
+                  value.combinaisons.map((combinaison, index) => (
+                    <li className='combinaison-maison' key={index}>{combinaison}</li>
+                  ))
+                }
+              </ul>
+            </li> ))
+          : <li>Pas de miroir . . .</li>
       }
+      </ul>
     </article>
   )
 }
 
 Miroirs.propTypes = {
-  miroirs: PropTypes.array,
+  miroirs: PropTypes.object,
   majors: PropTypes.array,
 }
 export default Miroirs;
