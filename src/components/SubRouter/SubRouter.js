a361352042ba497617e237b20d6316a01c52e9a8
Lucien Dulac <coulterfeith@live.fr>
2018-10-08 20:53:17 +0200

    subRouting configured as before, refactoring code and re-indentation made to be more readable

--- src/components/SubRouter/SubRouter.js
+++ src/components/SubRouter/SubRouter.js
@@ -2,45 +2,20 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { subRoutes } from './config';
 import Default from '../Default/Default';
-import Miroirs from '../Miroirs/miroirs';
-
 
 const SubRouter = (props) => (
   <Switch history={props.history}>
     {
-      subRoutes.map(({ path, exact, Component }) => (
+      props.birthday && subRoutes.map(({ path, exact, Component }) => (
         <Route
-          key={`subroute${path}`}
+          key={`subroute-${path}`}
           exact={exact}
-          path={props.match.url + path}
-          render={() => props.year
-            ? <Component {...props}/>
-            : <Default/> }
+          path={`${props.match.url}/${path}`}
+          render={() => <Component {...props} path={path}/>}
           />
       ))
     }
-    <Route key={`subroute/miroirs13`}
-          exact={true}
-          path={props.match.url + '/miroirs-13'}
-          render={() => props.year
-            ? <Miroirs miroirs={props.miroir13}/>
-            : <Default/> }
-          />
-    <Route key={`subroute/miroirs17`}
-          exact={true}
-          path={props.match.url + '/miroirs-17'}
-          render={() => props.year
-            ? <Miroirs miroirs={props.miroir17}/>
-            : <Default/> }
-          />
-    <Route key={`subroute/miroirs22`}
-          exact={true}
-          path={props.match.url + '/miroirs-22'}
-          render={() => props.year
-            ? <Miroirs miroirs={props.miroir22}/>
-            : <Default/> }
-          />
-    <Route from={"/"} to="*" component={Default}/>
+    <Route from="/" to="*" render={() => <Default {...props}/>}/>
   </Switch>
 );
 
--- src/components/SubRouter/config.js
+++ src/components/SubRouter/config.js
@@ -1,24 +1,43 @@
 import Arcanes from '../Arcanes/Arcanes';
 import VoiesBoucles from '../Voies_Boucles/Voies_Boucles';
 import AnneesPersonnelle from '../Annees/Annees';
+import Miroirs from '../Miroirs/miroirs';
 
 export const subRoutes = [
   {
-    path: '/arcanes',
+    path: 'arcanes',
     exact: true,
     Component: Arcanes,
     name: 'Arcanes'
   },
   {
-    path: '/voies-boucles',
+    path: 'voies-boucles',
     exact: true,
     Component: VoiesBoucles,
     name: 'Voies et Boucles',
   },
   {
-    path: '/annees-personnelle',
+    path: 'annees-personnelle',
     exact: true,
     Component: AnneesPersonnelle,
     name: 'Annees Personnelle',
+  },
+  {
+    path: 'miroirs13',
+    exact: true,
+    Component: Miroirs,
+    name: 'Miroirs à 13',
+  },
+  {
+    path: 'miroirs17',
+    exact: true,
+    Component: Miroirs,
+    name: 'Miroirs à 17',
+  },
+  {
+    path: 'miroirs22',
+    exact: true,
+    Component: Miroirs,
+    name: 'Miroirs à 22',
   },
 ];
