65d5fa3f4d1c73aad5d9ef760d9e29860529b33e
Lucien Dulac <coulterfeith@live.fr>
2018-10-08 20:50:38 +0200

    history import removed, attributs re-indented to be more readable

--- src/components/Router/Router.js
+++ src/components/Router/Router.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import history from '../../utils/history';
 import { appRoutes } from './config';
 
 const MainRouter = (props) => (
   <Switch>
     {
       appRoutes.map(({ path, exact, Component }) => (
-        <Route key={`route${path}`} exact={exact} path={path} render={({match}) => <Component match={match} history={history} {...props} />}/>
+        <Route
+          key={`route${path}`}
+          exact={exact}
+          path={path}
+          render={({ match }) => <Component match={match} {...props} />} />
       ))
     }
     <Redirect from="*" to="/individuel"/>
