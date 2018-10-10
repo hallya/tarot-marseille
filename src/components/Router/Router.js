513d251a90b9aaf8c8455f5cf109d8fd6dc284f8
Lucien Dulac <coulterfeith@live.fr>
2018-10-11 00:52:58 +0100

    Now route takes children, config has more options

--- src/components/Router/Router.js
+++ src/components/Router/Router.js
@@ -5,12 +5,17 @@ import { appRoutes } from './config';
 const MainRouter = (props) => (
   <Switch>
     {
-      appRoutes.map(({ path, exact, Component }) => (
+      appRoutes.map(({ path, exact, Component, Children }) => (
         <Route
           key={`route${path}`}
           exact={exact}
           path={path}
-          render={({ match }) => <Component match={match} {...props} />} />
+          render={({ match }) => (
+            <Component match={match} {...props}>
+              {Children}
+            </Component>
+          )}
+        />
       ))
     }
     <Redirect from="*" to="/individuel"/>
--- src/components/Router/config.js
+++ src/components/Router/config.js
@@ -1,15 +1,20 @@
-import Individuel from '../Individuel/Individuel';
-import Alliance from '../Alliance/Alliance';
-
+import React from 'react';
+import Main from '../Main/Main';
+import Fieldset from '../User/fieldset';
 export const appRoutes = [
   {
     path: '/individuel',
     exact: false,
-    Component: Individuel
+    Component: Main,
+    Children: <Fieldset firstname={'firstname'} lastname={'lastname'} birthday={'birthday'}/>
   },
   {
     path: '/alliance',
     exact: false,
-    Component: Alliance
+    Component: Main,
+    Children: [
+      <Fieldset firstname={'firstname'} lastname={'lastname'} birthday={'birthday'} />,
+      <Fieldset firstname={'firstnamePartner'} lastname={'lastnamePartner'} birthday={'birthdayPartner'}/>
+    ]
   }
 ];
